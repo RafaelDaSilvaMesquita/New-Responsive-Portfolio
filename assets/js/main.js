cb19f1033ae02440ef74fde9268f212a1c9131e4
RafaelDaSilvaMesquita <dasilvamesquitarafael@gmail.com>
2024-03-01 09:28:21 +0100

    Ajout parcours pro

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,26 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
+/*========== QUALIFICATION TABS ==========*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
 /*========== SERVICES MODAL ==========*/
 const modalViews = document.querySelectorAll('.services__modal'),
       modalBtns = document.querySelectorAll('.services__button'),
